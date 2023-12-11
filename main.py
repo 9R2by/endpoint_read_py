0fd5988380a6d0150743dd3901803db543d25476
9R2by <37716834+9R2by@users.noreply.github.com>
2023-12-11 19:37:51 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
+import re, socket
 
 
 class Endpoints:
@@ -6,6 +6,25 @@ class Endpoints:
         self.id = id
         self.ip = ip
         self.port = port
+
+
+def do_things(id, ip, port, endpoints_array):
+    print(f"Doing things with endpoint {id}")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', port))
+    server_socket.listen(1)
+
+    print(f"Server is listening on port {port}")
+
+    while True:
+        connection, address = server_socket.accept()
+        print(f"Connection from {address}")
+        data = connection.recv(1024)
+        if not data:
+            break
+        print(f"Received data: {data.decode('utf-8')}")
+        connection.sendall(b"Hello, client! I received your message.")
+        connection.close()
 
 
 def read_file_and_create_objects(file_path):
@@ -25,6 +44,13 @@ def read_file_and_create_objects(file_path):
 if __name__ == "__main__":
     file_path = 'endpoints.txt'
     endpoints_array = read_file_and_create_objects(file_path)
-
     for endpoint in endpoints_array:
         print(f"{endpoint.id}\t{endpoint.ip}\t{endpoint.port}")
+
+    user_input = input("Enter a unique id: ")
+    if len(endpoints_array) < 4:
+        print("Not enough endpoints")
+    else:
+        for endpoint in endpoints_array:
+            if endpoint.id == int(user_input):
+                do_things(endpoint.id, endpoint.ip, endpoint.port, endpoints_array)
