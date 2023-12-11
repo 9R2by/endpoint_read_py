24271b5efb5eb28471713c748d0f3ddfa00f5ddc
9R2by <37716834+9R2by@users.noreply.github.com>
2023-12-11 19:00:01 +0100

    Init files

---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+
+
+class Endpoints:
+    def __init__(self, id, ip, port):
+        self.id = id
+        self.ip = ip
+        self.port = port
+
+
+def read_file_and_create_objects(file_path):
+    endpoints_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            newlines_removed = line.rstrip("\n")
+            regex_result = re.split(r'[:\s]', newlines_removed)
+            if len(regex_result) == 3:
+                endpoint_id = int(regex_result[0])
+                ip_address = regex_result[1]
+                port = int(regex_result[2])
+                endpoints_list.append(Endpoints(endpoint_id, ip_address, port))
+    return endpoints_list
+
+
+if __name__ == "__main__":
+    file_path = 'endpoints.txt'
+    endpoints_array = read_file_and_create_objects(file_path)
+
+    # Print the result
+    for endpoint in endpoints_array:
+        print(f"{endpoint.id}\t{endpoint.ip}\t{endpoint.port}")
